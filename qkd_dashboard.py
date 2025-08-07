ffe89a612e7b2937286a0268c5df4b7ecf7680ce
Vasanth Rajarathinam <krvasanths@gmail.com>
2025-08-07 09:47:44 -0500

    Update qkd_dashboard.py

--- qkd_dashboard.py
+++ qkd_dashboard.py
@@ -56,6 +56,7 @@ for u, v in G.edges():
         original_qber = G[u][v].get('original_qber', G[u][v]['qber'])
         G[u][v]['qber'] = original_qber
 
+print(G)
 # Predict edge security using DQN agent
 edge_security = {}
 with torch.no_grad():
